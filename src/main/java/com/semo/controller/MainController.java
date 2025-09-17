db4766d1f6b54880b5f86d5a9d5e1cde45393e3f
hi-johng-bye <ckswhd1202@naver.com>
2025-09-17 14:22:13 +0900

    0917 1234

--- src/main/java/com/semo/controller/MainController.java
+++ src/main/java/com/semo/controller/MainController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/main")
-public class MainController {
-
-  @GetMapping(value = {"", "/"})
-  public String main() {
-    return "main/main";
+  @Controller
+  @RequestMapping("/main")
+  public class MainController {
+    //창고이음 소개 페이지
+    @GetMapping({"/introduce", "/introduce/"})
+    public String introduce() {
+      return "main/introduce";
+    }
   }
-}
